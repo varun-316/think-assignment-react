45d21807893500b574cefdecfda30eb844097272
varun-316 <varun.pandey@think360.ai>
2022-09-07 17:29:14 +0530

    UPDATE: Login form and data added

---
+++ src/data/loginFormData.js
@@ -0,0 +1,29 @@
+export const loginFormData = [
+  {
+    type: "text",
+    id: "uname",
+    name: "username",
+    className: "",
+    required: true,
+    placeholder: "Enter your Username",
+    value: "",
+  },
+  {
+    type: "password",
+    id: "pwd",
+    name: "password",
+    className: "",
+    required: true,
+    placeholder: "Enter your Password",
+    value: "",
+  },
+  {
+    type: "submit",
+    id: "login-submit",
+    name: "login-submit",
+    className: "",
+    required: true,
+    placeholder: "",
+    value: "Login",
+  },
+];
