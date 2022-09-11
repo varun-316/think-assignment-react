3cac577c7cbdd6ff9aba1102393ce2214257cde9
varun-316 <varun.pandey@think360.ai>
2022-09-11 18:59:18 +0530

    UPDATE: Added template on display page

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,15 +1,27 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { MainContext } from "./Home";
 
 const Details = () => {
   const [main] = useContext(MainContext);
-  const [details] = useState({
-    ...main,
-  });
 
   return (
     <React.Fragment>
-      <p>{JSON.stringify(details)}</p>
+      <h2>Username:</h2> {main.login.username} <br /> <br />
+      <h2>Personal Information:</h2>
+      <b>Name:</b> {`${main.personal.firstname} ${main.personal.lastname}`}{" "}
+      <br />
+      <b>Contact:</b> {main.personal.phone} <br />
+      <b>Email:</b> {main.personal.email} <br />
+      <b>Email:</b> {main.personal.email} <br />
+      <b>Address:</b> <br /> {main.address.addr1} <br /> {main.address.addr2}{" "}
+      <br />
+      <b>Pincode:</b> {main.address.pin}
+      <br />
+      <br />
+      <h2>Identification:</h2>
+      <b>Aadhaar Number:</b> {main.govt.aadhaar}
+      <br />
+      <b>PAN:</b> {main.govt.pan}
     </React.Fragment>
   );
 };
