e01c3c2bdaf4eb826fd3f18fd7f96a157a9ddce6
varun-316 <varun.pandey@think360.ai>
2022-09-11 19:16:51 +0530

    FIX: Removed unused import

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import Form from "../components/Form";
 import { loginFormData } from "../data/loginFormData";
 import { MainContext } from "./Home";
